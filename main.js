33c9f57f97cc95d9ab92ce67dcd1a858a854e580
arc12ace <71860574+arc12ace@users.noreply.github.com>
2021-02-18 20:44:31 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+Webcam.set({
+    width:300,
+    height:300,
+    image_format: "png",
+    png_quality:90
+
+});
+// code for attaching the webcam
+Webcam.attach("#camera")
+//code for taking snapshot
+function capture(){
+    Webcam.snap(function (picture) {
+        document.getElementById("snapshot").innerHTML=`<img id="captured_image" src=${picture}>`
+    })
+}
+//check the version of ml5
+console.log("ml5 version", ml5.version);
+
+//code for importing the model
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/uaGo71JYm/model.json", modelLoaded);
+
+//code for checking whther model has loaded or not
+function modelLoaded(){
+    console.log("model loaded successfully")
+}
+   
